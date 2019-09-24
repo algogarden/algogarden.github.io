b2ff8013a841ea446d6aa62dfebc0abdfe463afd
trung.hua <huavitrung16@gmail.com>
2019-09-24 18:14:13 +0700

    fix conflict

--- _site/assets/js/convex_hull.js
+++ _site/assets/js/convex_hull.js
@@ -3,20 +3,20 @@
 // 	//Fetch our canvas
 // 	var canvas = document.getElementById('world');
 	
-// 	//Setup Matter JS
-// 	var engine = Matter.Engine.create();
-// 	var world = engine.world;
-// 	var render = Matter.Render.create({
-// 		canvas: canvas,
-// 		engine: engine,
-// 		options: {
-// 			width: 500,
-// 			height: 500,
-// 			background: 'transparent',
-// 			wireframes: false,
-// 			showAngleIndicator: false
-// 		}
-// 	});
+	//Setup Matter JS
+	var engine = Matter.Engine.create();
+	var world = engine.world;
+	var render = Matter.Render.create({
+		canvas: canvas,
+		engine: engine,
+		options: {
+			width: 1080,
+			height: 500,
+			background: 'transparent',
+			wireframes: false,
+			showAngleIndicator: false
+		}
+	});
 	
 // 	//Add a ball
 // 	var ball = Matter.Bodies.circle(250, 250, 50, {
